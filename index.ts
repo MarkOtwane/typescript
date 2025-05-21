c4fe48ceba41b83d34aca0ccfde77ba440a27f08
KING DAVID <otwanemark254@gmail.com>
2025-05-21 05:03:46 +0300

    all information pertaining this project

--- index.ts
+++ index.ts
@@ -1,13 +1,10 @@
-interface User{
-    name:string;
-    id:number;
-}
-interface Admin extends User{
-    role:string;
-}
- const user: User ={name:'Mark', id:40918101};
- const admin: Admin = { name:"Mark", id:40918101, role:"Admin"}
-
- console.log(user)
- console.log(admin);
+// build task management system
+//2 classes user and task class
+//user class has the user deatils(name, role, id)
+//task (creating, updating, delete, retrive)
+// admin has a role of assigning task to the users, unassign and retrieve the information
+// for th system to run role if the role is admin then display the create,update, delete, retrieve data options (both users and the tasks)
+//now it gives two options user details or tasks details(if its user option creating, deleting, updating,retrieve)
+//if its task(option creating, deleting, updating,retrieve) then assign 
+//when the assign method is called all users are displayed in the terminal
  
